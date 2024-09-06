6c5b6eff3b7dbfd5bef53a841c348ffb8b52ea39
Kauan Lima <kauanalmeidalima1405@gmail.com>
2024-09-06 00:29:42 -0300

    Configurando o Acesso ao Backend

---
+++ src/hooks/useFoodData.ts
@@ -0,0 +1,12 @@
+import axios from "axios"
+
+const API_URL = 'http://localhost:8080'
+
+const fetchData = async (): =>{
+    const response = axios.get("API_URL" + '/food')
+    return
+}
+
+export function useFoodData(){
+
+}
