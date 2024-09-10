05f963743223cb40689d3fbdfafb7e832228dcd9
Kauan Lima <kauanalmeidalima1405@gmail.com>
2024-09-09 21:32:08 -0300

    Adicionando CSS na aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Card } from './components/card/card';
 import { FoodData } from './interface/FoodData';
+import { useFoodData } from './hooks/useFoodData';
 
 function App() {
-  const data: FoodData[] = [];
+  const { data } = useFoodData();
+
   return (
-    <div className='container'>
+    <div className="container">
       <h1>Cardápio</h1>
       <div className="card-grid">
-        {data.map(foodData => 
+        {data?.map(foodData => 
         <Card 
         price={foodData.price} 
         title={foodData.title} 
-        image={foodData.image}/>)}
+        image={foodData.image}
+        />
+        )}
       </div>
     </div>
   )
--- src/hooks/useFoodData.ts
+++ src/hooks/useFoodData.ts
@@ -1,12 +1,23 @@
-import axios from "axios"
+import axios, { AxiosPromise } from "axios"
+import { FoodData } from "../interface/FoodData";
+import { useQuery } from "@tanstack/react-query";
 
 const API_URL = 'http://localhost:8080'
 
-const fetchData = async (): =>{
-    const response = axios.get("API_URL" + '/food')
-    return
+const fetchData = async (): AxiosPromise <FoodData[]>=>{
+    const response = axios.get(API_URL + '/food')
+    return response;
 }
 
 export function useFoodData(){
+    const query = useQuery({
+        queryFn: fetchData,
+        queryKey: ['food-data'],
+        retry:2
+    })
 
+    return{
+        ...query,
+        data: query.data?.data
+    }
 }
