a380d0afd750eb1b5b29d848c0a7508d081feb4b
Kauan Lima <kauanalmeidalima1405@gmail.com>
2024-11-12 23:28:54 -0300

    Ajustando tela de login

--- src/login/Register.tsx
+++ src/login/Register.tsx
@@ -21,7 +21,7 @@ const Login: React.FC = () => {
         setError(null);
 
         try {
-            const response = await axios.post(`${url}/auth/register`, {
+            const response = await axios.post(`${url}auth/register`, {
                 login,
                 password,
                 role
